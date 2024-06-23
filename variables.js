8eec3ad261183eb8f95a92e2f77f637ecf1afbe3
Alejandro <maciasale.ctf@gmail.com>
2024-06-23 16:22:11 -0300

    inicio js

---
+++ variables.js
@@ -0,0 +1,29 @@
+let nombre = "ale";
+let apellido = "macias";
+
+// const dni =
+alert(nombre);
+alert(apellido);
+
+console.log(nombre);
+console.log(apellido);
+
+const documento = 2313123113;
+
+nombre = "marito";
+
+console.log(nombre);
+
+let edadDeLaPersona = 34;
+console.log(edadDeLaPersona);
+
+let numero = prompt("por favor ingresa un numero");
+
+console.log(numero);
+
+let n1 = Number(prompt("ingresa el primer numero"));
+let n2 = Number(prompt("ingresa el segundo numero"));
+
+let total = n1 + n2;
+
+console.log(total);
