8b3e9dbd64b7da10764819d75df87faf928ab481
Alejandro <maciasale.ctf@gmail.com>
2024-06-23 17:37:09 -0300

    cambios

---
+++ estructurasDeControl.js
@@ -0,0 +1,29 @@
+// una respuesta  a una pregunta
+
+let Nombre = prompt("ingresa tu nombre");
+let edad = Number(prompt("ingresa tu edad"));
+
+//alert("gracias por tu compra");
+
+let n1 = 5;
+let n2 = 12;
+
+console.log(n1 > n2); // false
+console.log(n1 < n2); // true
+console.log(n1 >= n2); // false
+console.log(n1 <= n2); // true
+console.log(n1 === n2); // false
+// n1 = 5
+// n2 = "5"
+//console.log(n1 !== n2);
+// n1 es distinta de xq n2 es string
+//(con esta forma js verifica al 100% el numero)
+
+alert("gracias por tu compra");
+
+// if( la condicion){
+//     lo que yo quiero hacer
+// }
+
+if (edad >= 18) {
+}
